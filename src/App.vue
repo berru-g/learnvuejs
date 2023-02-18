0e7cf87ae1bf0c15d3c8f88bd90ff1030902962e
berru-g <g.leberruyer@gmail.com>
2023-02-18 12:54:26 +0100

    first

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,12 @@
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
+<template>
+  <div>
+    <button @click="createPipette">Pipette</button>
+    <img src="..." @mousemove="handleMouseMove">
+  </div>
+</template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
